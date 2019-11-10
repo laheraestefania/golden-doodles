d46de32d946cbe64378d656443b78b7f13739159
Estefania Lahera <elahera@college.harvard.edu>
2019-11-10 00:36:14 -0500

    basic template and data

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// Start application by loading the data
+loadData();
+
+function loadData() {
+    // d3.csv("", function(error, jsonData){
+    //     if(!error){
+    //         createVis();
+    //     }
+    // });
+}
+
+
+function createVis() {
+	// TO-DO: Instantiate visualization objects here
+
+}
