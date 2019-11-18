1c27b6a6a70df3172300128742ba9579c1521bdf
Winnie Wang <winnie_wang@college.harvard.edu>
2019-11-17 21:21:15 -0500

    created static scatterplot for 2013 data only

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -10,7 +10,7 @@ Scatterplot = function(_parentElement, _data, _config){
     this.parentElement = _parentElement;
     this.data = _data;
     this.config = _config;
-    this.displayData = _data;
+    this.displayData = Object.values(_data);
 
     this.initVis();
 }
@@ -23,10 +23,10 @@ Scatterplot = function(_parentElement, _data, _config){
 Scatterplot.prototype.initVis = function(){
     var vis = this;
 
-    vis.margin = { top: 40, right: 40, bottom: 60, left: 60 };
+    vis.margin = { top: 30, right: 30, bottom: 60, left: 60 };
 
-    vis.width = 700 - vis.margin.left - vis.margin.right,
-        vis.height = 700 - vis.margin.top - vis.margin.bottom;
+    vis.width = 750 - vis.margin.left - vis.margin.right,
+        vis.height = 550 - vis.margin.top - vis.margin.bottom;
 
     // SVG drawing area
     vis.svg = d3.select("#" + vis.parentElement).append("svg")
@@ -44,19 +44,46 @@ Scatterplot.prototype.initVis = function(){
     vis.y = d3.scaleLinear()
         .range([vis.height, 0]);
 
+    vis.xAxis = d3.axisBottom()
+        .scale(vis.x);
+
     vis.yAxis = d3.axisLeft()
         .scale(vis.y);
 
     vis.svg.append("g")
+        .attr("class", "x-axis axis")
+        .attr("transform", "translate(0," + vis.height + ")");
+
+    vis.svg.append("g")
         .attr("class", "y-axis axis");
 
-    vis.svg.append("g").attr("class", "label");
+    // graph title
+    vis.svg.append("text").attr("class", "title")
+        .attr("x", 300)
+        .attr("y", vis.margin.top/4)
+        .attr("text-anchor", "middle")
+        .style("font-size", "16px")
+        .text("Fruit and Vegetable Availability vs GDP of Countries (2013)");
 
-    vis.svg.append("text").attr("class", "title");
+    // x-axis label
+    vis.svg.append('text')
+        .attr('x', vis.width / 2)
+        .attr('y', vis.height + vis.margin.bottom/1.5)
+        .attr('text-anchor', 'middle')
+        .attr("fill", "black")
+        .text('Gross Domestic Product');
+
+    // // y-axis label
+    vis.svg.append("text")
+        .text("Fruit and Vegetable Availability")
+        .attr("fill", "black")
+        .attr("x", -vis.height/1.5)
+        .attr("y", - vis.margin.left/2)
+        .attr("transform", "rotate(-90)");
 
     // // * TO-DO *
-
-    console.log(vis.data);
+    console.log("Original data", vis.data);
+    console.log(vis.displayData);
     // (Filter, aggregate, modify data)
     vis.wrangleData();
 }
@@ -70,18 +97,27 @@ Scatterplot.prototype.wrangleData = function(){
     var vis = this;
 
     // // * TO-DO *
-    vis.displayData = {};
+    // vis.displayData = {};
 
-    // Create a sequence from 0 - 14 (priorities: 1-15; array length: 15), initialize values to (0,0)
-    var fruitVegVersusGDP = d3.range(0, 15).map(function(){
-        return [0, 0];
-    });
+    // // Create a sequence from 0 - 14 (priorities: 1-15; array length: 15), initialize values to (0,0)
+    // var fruitVegVersusGDP = d3.range(0, 15).map(function(){
+    //     return [0, 0];
+    // });
+    //
+    // for (let id in vis.data) {
+    //     if (!isNaN(vis.data[id][vis.feature])) {
+    //         this.displayData[id] = vis.data[id][vis.feature];
+    //     }
+    // }
+    vis.displayData.forEach(function(d){
 
-    for (let id in vis.data) {
-        if (!isNaN(vis.data[id][vis.feature])) {
-            this.displayData[id] = vis.data[id][vis.feature];
+        if (isNaN(d.GDP_capita_PPP_2013)){
+            d.GDP_capita_PPP_2013 = 0;
         }
-    }
+        if (isNaN(d.fruit_veg_availability_2013)){
+            d.fruit_veg_availability_2013 = 0;
+        }
+    });
 
     // Update the visualization
     vis.updateVis();
@@ -96,8 +132,40 @@ Scatterplot.prototype.wrangleData = function(){
 Scatterplot.prototype.updateVis = function(){
     var vis = this;
 
-    // * TO-DO *
+    vis.x.domain([0, d3.max(vis.displayData, function(d) {return d.GDP_capita_PPP_2013; })]);
+    vis.y.domain([0, d3.max(vis.displayData, function(d) {return d.fruit_veg_availability_2013; })]);
 
+    // create tooltip using d3 library
+    vis.tooltip = d3.tip()
+        .attr("class", "d3-tip")
+        .offset([-8, 0])
+        .html(function(d){
+            return "Country: " + d.country +
+                "</br>  GDP Per Capita: " + d.GDP_capita_PPP_2013 +
+                "</br>  Food and Vegetable Availability: " + d.fruit_veg_availability_2013 ; });
+    vis.svg.call(vis.tooltip);
+
+    vis.colorPalette = d3.scaleOrdinal(d3.schemeCategory10);
+    vis.colorPalette.domain(["Europe", "Asia", "Latin America and the Caribbean","N. America", "Africa", "Oceania" ]);
+
+    vis.svg.selectAll("circle")
+        .data(vis.displayData)
+        .enter()
+        .append("circle")
+        .attr("fill", function(d){
+            return vis.colorPalette(d.region);
+        })
+        // add tooltip whenever mouse hovers over
+        .on("mouseover", vis.tooltip.show)
+        .on("mouseout", vis.tooltip.hide)
+        .attr("stroke", "black")
+        .attr("cx", function(d){ return vis.x(d.GDP_capita_PPP_2013); })
+        .attr("cy", function(d){ return vis.y(d.fruit_veg_availability_2013); })
+        .attr("r", 2);
+
+    // Call axis functions with the new domain
+    vis.svg.select(".x-axis").call(vis.xAxis);
+    vis.svg.select(".y-axis").call(vis.yAxis);
 
 
 }
